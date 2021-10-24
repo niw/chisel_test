4ebf4be73cab2f6f7b105b5172bacebc693bfb85
Yoshimasa Niwa <niw@niw.at>
2021-10-24 16:25:01 -0700

    Simplify Add.

--- src/main/scala/Add.scala
+++ src/main/scala/Add.scala
@@ -9,20 +9,17 @@ class Add(val w: Int) extends Module {
     val carryOut = Output(UInt(1.W))
   })
 
-  private val fullAdderIOs = VecInit(Seq.fill(w) {
-    Module(new FullAdder).io
-  })
   private val sums = Wire(Vec(w, UInt(1.W)))
   private val carries = Wire(Vec(w + 1, UInt(1.W)))
 
   carries(0) := io.carryIn
   for (i <- 0 until w) {
-    val fullAdderIO = fullAdderIOs(i)
-    fullAdderIO.a := io.a(i)
-    fullAdderIO.b := io.b(i)
-    fullAdderIO.carryIn := carries(i)
-    sums(i) := fullAdderIO.sum
-    carries(i + 1) := fullAdderIO.carryOut
+    val fullAdder = Module(new FullAdder)
+    fullAdder.io.a := io.a(i)
+    fullAdder.io.b := io.b(i)
+    fullAdder.io.carryIn := carries(i)
+    sums(i) := fullAdder.io.sum
+    carries(i + 1) := fullAdder.io.carryOut
   }
 
   io.sum := sums.asUInt()
